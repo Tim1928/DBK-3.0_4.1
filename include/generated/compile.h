16a6ac848b64bfb23dc406a0d0bbce3ae02d9a28
Tim1928CM7 <timpuckett2007@gmail.com>
2013-02-13 03:11:20 -0600

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Feb 12 13:31:41 CST 2013"
+#define LINUX_COMPILE_BY "tim1928"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.62-cyanogenmod-gb07d01b-dirty"
